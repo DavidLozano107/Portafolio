210a4e46353aa84ff44f32a764577455b06dc72f
David lozano <Davidlozano107@gmail.com>
2020-09-05 14:36:00 -0500

    Parte principal

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const x = window.matchMedia("(max-width: 1600px)");
+function cambiarImg(x) {
+    if (x.matches) {
+        // If media query matches
+        document.querySelector(".svg-developer").src =
+            "./css/img/Social-Network/Developer2.svg";
+    } else {
+        document.querySelector(".svg-developer").src =
+            "./css/img/Social-Network/Developer.svg";
+    }
+}
+
+cambiarImg(x); // Call listener function at run time
+x.addListener(cambiarImg); // Attach listener function on state changes
